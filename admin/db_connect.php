76904a361b5d6b8bc90a057aa70749295507aa66
Ragyo <izwael_1997@hotmail.com>
2023-01-14 18:34:25 -0600

    cambios hechizos

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,7 +1,2 @@
 <?php 
-
-<<<<<<< HEAD
-$conn= new mysqli('localhost','root','','flight_booking_db')or die("Could not connect to mysql".mysqli_error($con));
-=======
 $conn= new mysqli('localhost','root','','hoteljj')or die("Could not connect to mysql".mysqli_error($con));
->>>>>>> 21582ace24a182502792642b4f47b15320a5d209
